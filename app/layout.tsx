1de4feab876e8ca9039fad4cc948606500af16a8
TakudzwaMbanois <mbanotakudzwa2001@gmail.com>
2026-06-27 21:29:53 +0200

    icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,20 +63,7 @@ export const metadata: Metadata = {
   },
   generator: 'Next.js',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/icon.png',
     apple: '/apple-icon.png',
   },
 }
